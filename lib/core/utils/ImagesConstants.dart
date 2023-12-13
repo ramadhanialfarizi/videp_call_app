ffed49fdc14d4f9b58446ba62cf000388c6d53b2
Muhammad Ramadhani Alfarizi <muhammad.alfarizi@digitalcenter.id>
2023-12-13 17:45:11 +0700

    update login page

--- lib/core/utils/ImagesConstants.dart
+++ lib/core/utils/ImagesConstants.dart
@@ -2,7 +2,12 @@
 
 class ImagesConstants {
   static const String _gift = 'assets/gift/';
+  static const String _icons = 'assets/icons/';
 
   // GIFT
   static const String appIconsGift = '${_gift}app_logo.gif';
+
+  // ICONS
+  static const String appIcon = '${_icons}app_icon.png';
+  static const String googleIcon = '${_icons}google_icon.png';
 }
--- lib/features/onboarding/Login/view/loginScreen.dart
+++ lib/features/onboarding/Login/view/loginScreen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:video_call_app/core/utils/ImagesConstants.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
@@ -6,9 +7,33 @@ class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [],
+        body: Center(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.asset(
+            ImagesConstants.appIcon,
+            scale: 3,
+          ),
+          const SizedBox(
+            height: 15,
+          ),
+          ElevatedButton.icon(
+            style: ButtonStyle(
+              padding: MaterialStateProperty.all<EdgeInsetsGeometry?>(
+                const EdgeInsets.all(16.0),
+              ),
+            ),
+            onPressed: () {},
+            icon: Image.asset(
+              ImagesConstants.googleIcon,
+              scale: 100,
+            ),
+            label: Text("Login with Google"),
+          ),
+        ],
       ),
-    );
+    ));
   }
 }
